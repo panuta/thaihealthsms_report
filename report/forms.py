4128698d2f058398456aaa0a2cb14df91103d6ee
Panu Tangchalermkul <panuta@gmail.com>
2012-01-24 19:52:19 +0700

    fix report overview page

--- report/forms.py
+++ report/forms.py
@@ -5,7 +5,7 @@ from django import forms
 from common.forms import StrippedCharField
 
 class SubmitReportTextForm(forms.Form):
-    report_text = StrippedCharField()
+    report_text = StrippedCharField(required=False)
 
 class SubmitReportAttachmentForm(forms.Form):
     report_attachment = forms.FileField()
